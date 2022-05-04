827fd6f0f676d03c9d3a90f779ff177d2e51d4ed
mitanvir05 <tanvir.cse05@gmail.com>
2022-05-04 15:34:36 +0600

    single data showing

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
-//database name :inventory
-//FxADEGUKHu40WQje
-
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 //middleware
 app.use(cors());
 app.use(express.json());
-app.get("/", (req, res) => {
-  res.send("running server dfsggdsgsdg");
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jrbph.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
+
+app.get("/", (req, res) => {
+  res.send("running server");
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const inventoryCollection = client.db("inventorydata").collection("user");
+    app.get("/inventory", async (req, res) => {
+      const query = {};
+      const cursor = inventoryCollection.find(query);
+      const inventories = await cursor.toArray();
+      res.send(inventories);
+    });
+
+    app.get("/inventory/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const inventory = await inventoryCollection.findOne(query);
+      res.send(inventory);
+    });
+  } finally {
+  }
+}
+run().catch(console.mkdir);
+
 app.listen(port, () => {
   console.log("listening", port);
 });
