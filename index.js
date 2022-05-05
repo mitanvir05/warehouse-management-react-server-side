8f5c9b53558df660c3883c554a0e585f03d6139f
mitanvir05 <tanvir.cse05@gmail.com>
2022-05-06 03:20:37 +0600

    post method added

--- index.js
+++ index.js
@@ -36,6 +36,12 @@ async function run() {
       const inventory = await inventoryCollection.findOne(query);
       res.send(inventory);
     });
+    //POST
+    app.post("/inventory", async (req, res) => {
+      const newInventory = req.body;
+      const result = await inventoryCollection.insertOne(newInventory);
+      res.send(result);
+    });
   } finally {
   }
 }
